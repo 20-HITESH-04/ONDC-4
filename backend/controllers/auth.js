6354202c8be45d3ce534166aa4f0b4bddd8a81a6
Nitish Sharma <nit4642@gmail.com>
2025-01-14 18:38:58 +0530

    signup, login with jwt done

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,12 +5,9 @@ import User from "../models/User.js";
 /* REGISTER USER */
 export const register = async (req, res) => {
   try {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-    } = req.body;
+    console.log(req.body);
+
+    const { firstName, lastName, email, password } = req.body;
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
 
 const PORT = 3001;
 
@@ -7,12 +8,13 @@ const app = express();
 
 
 
+app.use(express.json());
+app.use("/auth", authRoutes);
 
 
 
-
-app.listen(PORT, () => {
-    connectDB();
-    console.log("Server is running on port 3000");
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log("Server is running on port 3001");
     }
 );
